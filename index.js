d542475131041b85fff597c4fc8ded8ea23eb2bf
Markov Roman <markovrv@yandex.ru>
2022-12-05 23:50:18 +0300

    set

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const http = require("http");
- 
-let message = "Hello World!!!";
-http.createServer(function(request,response){
+// подключение express
+const express = require("express");
+// создаем объект приложения
+const app = express();
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
      
-    console.log(message);
-    response.end(message);
-     
-}).listen(3000, "127.0.0.1",()=>{
-    console.log("Сервер начал прослушивание запросов");
+    // отправляем ответ
+    response.send("<h2>Привет Express!</h2>");
 });
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
