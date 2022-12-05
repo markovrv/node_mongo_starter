e75244584f54e30bf1ac22ed0cd8f4517c948486
Markov Roman <markovrv@yandex.ru>
2022-12-05 23:54:16 +0300

    set

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
  
 const app = express();
+app.use(function(request, response, next){
+     
+    console.log("Middleware 1");
+    next();
+});
+app.use(function(request, response, next){
+     
+    console.log("Middleware 2");
+    next();
+});
+ 
 app.get("/", function(request, response){
      
-    response.send("<h1>Главная страница</h1>");
-});
-app.get("/about", function(request, response){
-     
-    response.send("<h1>О сайте</h1>");
-});
-app.get("/contact", function(request, response){
-     
-    response.send("<h1>Контакты</h1>");
+    console.log("Route /");
+    response.send("Hello");
 });
 app.listen(3000);
