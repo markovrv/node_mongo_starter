b7755fca7e8109d2ca7b0e1f5eddfa953fdcc4d9
Markov Roman <markovrv@yandex.ru>
2022-12-05 23:55:41 +0300

    set

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
+const fs = require("fs");
  
 const app = express();
 app.use(function(request, response, next){
      
-    console.log("Middleware 1");
-    next();
-});
-app.use(function(request, response, next){
-     
-    console.log("Middleware 2");
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
+    console.log(data);
+    fs.appendFile("server.log", data + "\n", function(){});
     next();
 });
  
 app.get("/", function(request, response){
-     
-    console.log("Route /");
     response.send("Hello");
 });
 app.listen(3000);
